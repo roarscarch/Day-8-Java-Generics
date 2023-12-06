3ad56a99b8a9a21f15cf8b28cbabeb9df6617474
roarscarch <2020ugee082@nitjsr.ac.in>
2023-12-06 06:27:23 +0530

    3 integers maximum

--- Generics.java
+++ Generics.java
@@ -1,6 +1,21 @@
-class Generics{
+public class Generics {
+     public static int getMaxInt(int a, int b, int c) {
+        if(a>b && a>c)return a;
+        else if (b>a && b>c)return b;
+        else return c;    
+    }
     public static void main(String[] args) {
-        System.out.println("Today we will study java generics");
+        System.out.println("Welcome to Generics Min-Max program.");
+
+       
+        System.out.println(getMaxInt(23, 43, 1));
+        System.out.println(getMaxInt(99, 35, 42));
+        System.out.println(getMaxInt(33, 81, 89));
+        System.out.println(getMaxInt(45, -1, 29));
+      
+
     }
 
+    
+   
 }
