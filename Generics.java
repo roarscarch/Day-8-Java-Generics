3742b38125d5622389c186bbefc8b904cd388715
roarscarch <2020ugee082@nitjsr.ac.in>
2023-12-06 06:50:10 +0530

    compare 3 strings

--- Generics.java
+++ Generics.java
@@ -2,6 +2,13 @@
  * Bhai float ke .f lagana padta hai
  * 0.9f 0.8f 
  * double ke liye d . 0.88d
+ * 
+ * compareTo method compares lexicographically in case of string 
+ * it returns 3 values 1 , -1 and 0
+ * when str1 is less than str2 compare to returns -1
+ * else if str1 is greater than str2 compare to returns 1
+ * else if str1 is equal to str2 compare to returns 0
+ * 
  */
 
 public class Generics {
@@ -17,11 +24,17 @@ public class Generics {
         else return c;
     }
 
+     public static String getMaxString(String a, String b, String c) {
+        if (a.compareTo(b) > 0 && a.compareTo(c) > 0) return a;
+        else if (b.compareTo(a) > 0 && b.compareTo(c) > 0) return b;
+        else return c;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Generics Min-Max program.");
 
-        System.out.println(getMaxFloat(233.3f, 43.4f, 1.0f));
-        System.out.println(getMaxFloat(99.0f, 355.5f, 42.9f));
-        System.out.println(getMaxFloat(33.7f, 81.2f, 9999.9f));
+        System.out.println(getMaxString("orange", "banana", "apple"));
+        System.out.println(getMaxString("dog", "elephant", "cat"));
+        System.out.println(getMaxString("java", "javascript", "python"));
     }
 }
