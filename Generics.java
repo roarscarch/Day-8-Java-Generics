577a050d43dbcf3b3ddb5965edf6968c04ffd745
roarscarch <2020ugee082@nitjsr.ac.in>
2023-12-06 06:36:39 +0530

    3 float maximum

--- Generics.java
+++ Generics.java
@@ -1,21 +1,27 @@
+/*
+ * Bhai float ke .f lagana padta hai
+ * 0.9f 0.8f 
+ * double ke liye d . 0.88d
+ */
+
 public class Generics {
-     public static int getMaxInt(int a, int b, int c) {
-        if(a>b && a>c)return a;
-        else if (b>a && b>c)return b;
-        else return c;    
+    public static int getMaxInt(int a, int b, int c) {
+        if (a > b && a > c) return a;
+        else if (b > a && b > c) return b;
+        else return c;
     }
+
+    public static float getMaxFloat(float a, float b, float c) {
+        if (a > b && a > c) return a;
+        else if (b > a && b > c) return b;
+        else return c;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Generics Min-Max program.");
 
-       
-        System.out.println(getMaxInt(233, 43, 1));
-        System.out.println(getMaxInt(99, 355, 42));
-        System.out.println(getMaxInt(33, 81, 9999));
-       
-      
-
+        System.out.println(getMaxFloat(233.3f, 43.4f, 1.0f));
+        System.out.println(getMaxFloat(99.0f, 355.5f, 42.9f));
+        System.out.println(getMaxFloat(33.7f, 81.2f, 9999.9f));
     }
-
-    
-   
 }
